809d2cb2f9563519706a2b40244f5b464814645e
ggoodspa20-blip <ggoodspa20@gmail.com>
2026-09-24 00:12:57 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+const APP_CONFIG = {
+    // นำ URL ที่ได้จากการ Deploy GAS (ที่ลงท้ายด้วย /exec) มาวางที่นี่
+    GAS_URL: "https://script.google.com/macros/s/AKfycbzJgy3xSR3uCEhTdg9Gnhh5Pnhmz58nR-nFSWWYFKjsF1Cw7qA7Xp_1kuUJiVo2S4zu/exec",
+    
+    // ใส่ LIFF ID ที่ตั้งค่าไว้ใน LINE Developers
+    LIFF_ID: "ใส่_LIFF_ID_ที่นี่"
+};
