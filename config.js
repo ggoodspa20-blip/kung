7fa453a3e8d52c5bab6e55658064a4942ed809e8
ggoodspa20-blip <ggoodspa20@gmail.com>
2026-09-26 05:41:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ const APP_CONFIG = {
     GAS_URL: "https://script.google.com/macros/s/AKfycbzJgy3xSR3uCEhTdg9Gnhh5Pnhmz58nR-nFSWWYFKjsF1Cw7qA7Xp_1kuUJiVo2S4zu/exec",
     
     // ใส่ LIFF ID ที่ตั้งค่าไว้ใน LINE Developers
-    const INDEX_LIFF_ID = "2009043114-xI8nWTT5"; // สำหรับผู้เล่น
-    const ADMIN_LIFF_ID = "2000218686-sKmiajH7"; // สำหรับแอดมิน
+    const INDEX_LIFF_ID = "2009043114-xI8nWTT5" // สำหรับผู้เล่น
+    const ADMIN_LIFF_ID = "2000218686-sKmiajH7" // สำหรับแอดมิน
