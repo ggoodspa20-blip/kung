2ef20b376f7a4e287a1a40f34effa0bb78b7ee20
ggoodspa20-blip <ggoodspa20@gmail.com>
2026-09-26 16:48:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,12 @@
 const APP_CONFIG = {
+  // URL ของ Google Apps Script (ยังเก็บไว้ใช้สำหรับยิง LINE Push Message หรือทำ Backup)
   GAS_URL: "https://script.google.com/macros/s/AKfycbzJgy3xSR3uCEhTdg9Gnhh5Pnhmz58nR-nFSwWYFkJsF1Cw7qA7Xp_1kuUJiVo2S4zu/exec",
-  LIFF_ID: "2009043114-xI8nWTT5",       // สำหรับหน้าผู้เล่น (index.html)
-  LIFF_ID_ADMIN: "2009043114-EfiHSaA5"  // สำหรับหน้าแอดมิน (admin.html)
+  
+  // LINE LIFF IDs
+  LIFF_ID: "2009043114-xI8nWTT5",       
+  LIFF_ID_ADMIN: "2009043114-EfiHSaA5",
+
+  // เพิ่มข้อมูล Supabase ของคุณตรงนี้
+  SUPABASE_URL: "https://wotpzelzwibohfgchtar.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_28HPs7-e0Z_hqO5vJ4fPjg_U81pPUr0"
 };
