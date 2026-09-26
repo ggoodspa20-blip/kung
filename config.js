9c856f5b4fb06692c6fa71cf4787d7213e506901
ggoodspa20-blip <ggoodspa20@gmail.com>
2026-09-26 16:04:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,5 @@
 const APP_CONFIG = {
-    // นำ URL ที่ได้จากการ Deploy GAS (ที่ลงท้ายด้วย /exec) มาวางที่นี่
-    GAS_URL: "https://script.google.com/macros/s/AKfycbzJgy3xSR3uCEhTdg9Gnhh5Pnhmz58nR-nFSWWYFKjsF1Cw7qA7Xp_1kuUJiVo2S4zu/exec";
-    
-    // LIFF ID สำหรับหน้าผู้เล่น (index.html)
-  LIFF_ID_PLAYER: "2009043114-xI8nWTT5",
-  
-  // LIFF ID สำหรับหน้าแอดมิน (admin.html)
-  LIFF_ID_ADMIN: "2009043114-EfiHSaA5"
+  GAS_URL: "https://script.google.com/macros/s/AKfycbzJgy3xSR3uCEhTdg9Gnhh5Pnhmz58nR-nFSwWYFkJsF1Cw7qA7Xp_1kuUJiVo2S4zu/exec",
+  LIFF_ID: "2009043114-xI8nWTT5",       // สำหรับหน้าผู้เล่น (index.html)
+  LIFF_ID_ADMIN: "2009043114-EfiHSaA5"  // สำหรับหน้าแอดมิน (admin.html)
 };
